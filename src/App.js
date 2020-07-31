583aa7a7d5c3e1f9a69de423495f10c255226c50
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-30 21:08:50 -0700

    feat: add state to input field and detetct button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+//Components
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: 'c5799ad4c8dd4d7ca5099a331b35f299'
+});
 
 const particlesOptions = {
   particles: {
@@ -19,6 +27,36 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl :''  
+    };
+  }
+  onInputChange = (event) => {
+    // console.log(event.target.value);
+    this.setState({input : event.target.value})
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input }); //update image url with the given input
+    console.log('click');
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input
+      )
+      .then(
+        function(response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+  };
+
   render() {
     return (
       <div className="App">
@@ -26,8 +64,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
